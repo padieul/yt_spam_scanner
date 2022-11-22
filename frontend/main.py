28fc459dd8814bdf351d03243bf845bf5c5fb68a
Paul Dietze <prdietze@outlook.de>
2022-11-23 00:08:16 +0100

    initial architecture scaffold - not functional yet

---
+++ frontend/main.py
@@ -0,0 +1,31 @@
+import tornado.ioloop
+from pywebio import start_server
+import tornado.web
+from pywebio.platform.tornado import webio_handler
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
+
+
+def bmi():
+    height = input("Your Height(cm)?", type=FLOAT)
+    weight = input("Your Weight(kg)?", type=FLOAT)
+
+    BMI = weight / (height / 100) ** 2
+
+    top_status = [
+        (14.9, "Severely underweight"),
+        (18.4, "Underweight"),
+        (22.9, "Normal"),
+        (27.5, "Overweight"),
+        (40.0, "Moderately obese"),
+        (float("inf"), "Severely obese"),
+    ]
+
+    for top, status in top_status:
+        if BMI <= top:
+            put_text("Your BMI: %.1f, category: %s" % (BMI, status))
+            break
+
+
+if __name__ == "__main__":
+    start_server(bmi, port=8000, address="localhost")
