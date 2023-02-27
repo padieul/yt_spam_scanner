4a5b1909619277fca19280d24e805eb7401d4425
Vivian Kazakova <the.vivkaz@gmail.com>
2023-02-27 13:51:51 +0100

    clean middleware classifier

--- middleware/app/classifier.py
+++ middleware/app/classifier.py
@@ -3,7 +3,7 @@ import glob
 import joblib
 import spacy
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class GenericClassifier:
@@ -11,39 +11,26 @@ class GenericClassifier:
     def __init__(self, model_path="saved_models/logisticregression_34-23.joblib"):
         self.model, self.model_name = self._load_model(model_path)
         self.corpus = self._load_corpus()
-        #self.vectorizer = CountVectorizer(binary=True, max_df=0.95) # TODO tfidf? TODO arguments?
         self.tfidf_vectorizer = TfidfVectorizer(max_df=0.95, use_idf=True, stop_words='english')
-        #self._download_nltk_packages()
-        self.nlp = spacy.load("en_core_web_sm", disable=["ner"]) # TODO spacy instaed of nltk?
+        self.nlp = spacy.load("en_core_web_sm", disable=["ner"])
         self._preprocess_corpus()
 
+
     def _load_model(self, path):
-        """
-        Load stored classifier
-        """
+        """Load stored classifier"""
         #path = "saved_models/multinomialnb_33-38.joblib" # TODO decide on trained classifier
         return joblib.load(path), (path[path.index("/")+1:][:path[path.index("/")+1:].index("_")])
 
+
     def _load_corpus(self, data_path = "data/"):
-        """
-        Load YouTube spam collection (kaggle dataset) from given path
-        """
-        #data_path = r"data/YouTube-Spam-Collection/"
+        """Load own YouTube comments collection from the given path"""
+        #data_path = r"data/YouTube-Spam-Collection/" # dataset from kaggle (1,956 comments from 5 videos among the 10 most viewed on the collection period)
         files = glob.glob(os.path.join(data_path, "*.csv"))
         return pd.concat((pd.read_csv(file) for file in files), ignore_index=True)
 
-    """def _download_nltk_packages(self):
-        #Download required nltk packages
-        nltk.download("punkt")
-        nltk.download("stopwords")
-        nltk.download('omw-1.4')
-        nltk.download("wordnet")"""
-
 
     def _preprocess_corpus(self):
-        """
-        Preprocessing pipeline for YouTube spam collection
-        """
+        """Preprocessing pipeline for the YouTube comments collection: provides lowercasing, spacy tokenization and lemmatization, and removes stopwords"""
         # remove blank rows
         self.corpus.dropna(inplace=True)
 
@@ -53,34 +40,26 @@ class GenericClassifier:
         # lowercase
         self.corpus['REPR'] = self.corpus['REPR'].str.lower()
 
+        # lemmatize, remove stopwords
         for comment in self.corpus["REPR"]:
             doc = self.nlp(comment)
-            # lemmatize, remove stopwords
             comment = " ".join([token.lemma_ for token in doc if not token.is_stop]) # TODO not token.is_punct?
-       
-        self.tfidf_vectorizer.fit_transform(self.corpus["REPR"]) # TODO tfid or countVectorizer?
-        #self.vectorizer.fit_transform(self.corpus["REPR"])
+
+        self.tfidf_vectorizer.fit_transform(self.corpus["REPR"])
+
 
     def _preprocess_single_comment(self, comment):
-        """
-        Preprocessing pipeline for given comment
-        """
+        """Preprocessing pipeline for given comment: provides lowercasing, spacy tokenization and lemmatization, and removes stopwords"""
         return " ".join([token.lemma_ for token in self.nlp(comment) if not token.is_stop]) # TODO not token.is_punct?
 
-    
+
     def _get_single_comment_embedding(self, comment):
+        """Return embedding for a given comment"""
         return self.tfidf_vectorizer.transform([self._preprocess_single_comment(comment)])
-        #return self.vectorizer.transform([self._preprocess_single_comment(comment)])
 
 
     def predict_single_comment(self, comment):
-        """
-        Classify the comment using the loaded classifier/model
-        """
-        arg = self._get_single_comment_embedding(comment)
-        print("arg")
-        print(arg)
-        prediction_label = self.model.predict(arg)
-        print("prediction label")
-        print(prediction_label)
+        """Classify the given comment using the loaded classifier/model"""
+        emb = self._get_single_comment_embedding(comment)
+        prediction_label = self.model.predict(emb)
         return { f"{self.model_name}_prediction": prediction_label }
